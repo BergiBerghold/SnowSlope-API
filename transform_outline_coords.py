51ec0a779a24ec746e15841d57ac6821e5447991
minimilch <natrium-linux>
2022-03-21 14:07:10 +0100

    test

--- transform_outline_coords.py
+++ transform_outline_coords.py
@@ -31,6 +31,5 @@ with open('outline_coords.txt') as input:
         out_array.append([lat, lng])
 
 print(out_array)
-print("GG")
 
-    #with open('outline_coords_wgs84.txt', 'w') as output:
+#with open('outline_coords_wgs84.txt', 'w') as output:
