37a2cad2a526cc6c8599e9e7fe86d345b8a2f2ce
minimilch <natrium-linux>
2022-04-07 11:57:43 +0200

    Rewriting main to be more testable

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,9 @@
-def test_amk():
-    assert 1 == 1
+from osgeo.gdalconst import *
+from osgeo import gdal, osr
+import numpy as np
+from main import *
+
+
+def test_load_elevation_model():
+    datasource = gdal.Open('test_dgms/test_dgm.tif')
+    databand = datasource.GetRasterBand(1)
