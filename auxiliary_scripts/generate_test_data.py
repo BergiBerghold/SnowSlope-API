b9fb4f5e8c9001cc8caa0093bb7975bac33e17e6
minimilch <natrium-linux>
2022-04-07 19:10:46 +0200

    Wrote tests and test generation

--- auxiliary_scripts/generate_test_data.py
+++ auxiliary_scripts/generate_test_data.py
@@ -1,12 +1,83 @@
-from osgeo.gdalconst import *
-from osgeo import gdal, osr
-import numpy as np
-from main import *
+from skislope_api.main import *
+import random
 
-datasource = gdal.Open('tests/test_dgms/test_dgm.tif')
-databand = datasource.GetRasterBand(1)
 
-elevation_model_array = databand.ReadAsArray().astype('float32')
-print(elevation_model_array.shape)
+def generate_test_data_calculate_tile_dimensions():
+    datasource = gdal.Open('tests/test_dgms/test_dgm.tif')
+    databand = datasource.GetRasterBand(1)
 
-print(calculate_tile_dimensions(databand, (534, 1341), 500))
+    elevation_model_array = databand.ReadAsArray().astype('float32')
+
+    parameter_array = []
+
+    for i in range(10):
+        x = random.randrange(1600)
+        y = random.randrange(2500)
+        endpoint_in = x, y
+
+        tilesize = random.randrange(1, 1400)
+
+        tile_dimensions, endpoint_out, warning_code = calculate_tile_dimensions(databand, (x, y), tilesize)
+
+        parameter_array.append((endpoint_in, tilesize, tile_dimensions, endpoint_out, warning_code))
+
+    print(parameter_array)
+
+
+def generate_test_data_load_elevation_model():
+    datasource = gdal.Open('tests/test_dgms/test_dgm.tif')
+    databand = datasource.GetRasterBand(1)
+
+    parameter_array = []
+
+    while len(parameter_array) < 10:
+        x = random.randrange(1600)
+        y = random.randrange(2500)
+        endpoint_in = x, y
+
+        tilesize = random.randrange(1, 5)
+
+        tile_dimensions, endpoint_out, warning_code = calculate_tile_dimensions(databand, (x, y), tilesize)
+
+        if tile_dimensions:
+            array = load_elevation_model(databand, tile_dimensions)
+            parameter_array.append((tile_dimensions, array.tolist()))
+
+    print(parameter_array)
+
+
+def generate_test_data_transform_wgs84_to_pixel():
+    datasource = gdal.Open('tests/test_dgms/test_dgm.tif')
+    databand = datasource.GetRasterBand(1)
+
+    parameter_array = []
+
+    for i in range(10):
+        lat = random.randrange(47155517, 47168500) / 10**6
+        long = random.randrange(12774348, 12805801) / 10**6
+
+        x, y = transform_wgs84_to_pixel(datasource, lat, long)
+
+        parameter_array.append((lat, long, x, y))
+
+    print(parameter_array)
+
+
+def generate_test_data_check_if_tile_too_small():
+    parameter_array = []
+
+    while len(parameter_array) < 5:
+        array = np.random.randint(0, 2, (10, 10))
+
+        array[0, :] = 0
+        array[:, 0] = 0
+        array[:, -1] = 0
+
+        if check_if_tile_too_small(array):
+            parameter_array.append(array.tolist())
+
+    print(parameter_array)
+
+
+generate_test_data_check_if_tile_too_small()
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,60 @@
-import sys
-sys.path.append('../SkiSlope')
+import tests.test_parameters as test_parameters
+from skislope_api.main import *
+import pytest
 
-from osgeo.gdalconst import *
-from osgeo import gdal, osr
-import numpy as np
-from main import *
+test_dgm_path = 'test_dgms/test_dgm.tif'
+
+'''
+Test for function calculate_tile_dimensions
+'''
+
+parameters = test_parameters.calculate_tile_dimensions
 
 
-def test_load_elevation_model():
-    datasource = gdal.Open('tests/test_dgms/test_dgm.tif')
+@pytest.mark.parametrize('endpoint_in, tilesize, tile_dimensions, endpoint_out, warning_code', parameters)
+def test_calculate_tile_dimensions(endpoint_in, tilesize, tile_dimensions, endpoint_out, warning_code):
+    datasource = gdal.Open(test_dgm_path)
     databand = datasource.GetRasterBand(1)
+
+    assert (tile_dimensions, endpoint_out, warning_code) == calculate_tile_dimensions(databand, endpoint_in, tilesize)
+
+
+'''
+Test for function load_elevation_model
+'''
+
+parameters = test_parameters.load_elevation_model
+
+
+@pytest.mark.parametrize('tile_dimensions, elevation_model_array', parameters)
+def test_load_elevation_model(tile_dimensions, elevation_model_array):
+    datasource = gdal.Open(test_dgm_path)
+    databand = datasource.GetRasterBand(1)
+
+    assert elevation_model_array == load_elevation_model(databand, tile_dimensions).tolist()
+
+
+'''
+Test for function transform_wgs84_to_pixel
+'''
+
+parameters = test_parameters.transform_wgs84_to_pixel
+
+
+@pytest.mark.parametrize('lat, long, x, y', parameters)
+def test_transform_wgs84_to_pixel(lat, long, x, y):
+    datasource = gdal.Open(test_dgm_path)
+
+    assert (x, y) == transform_wgs84_to_pixel(datasource, lat, long)
+
+
+'''
+Test for function check_if_tile_too_small
+'''
+
+parameters = test_parameters.check_if_tile_too_small
+
+
+@pytest.mark.parametrize('array, output', parameters)
+def test_check_if_tile_too_small(array, output):
+    assert output == check_if_tile_too_small(np.asarray(array))
